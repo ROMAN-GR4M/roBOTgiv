e2d33d837a774be62708b60529c10bc21824e1bf
ROMAN_GR4M <indexman1000@gmail.com>
2021-01-14 00:03:47 +0100

    roBOT

---
+++ commands/giveaway.js
@@ -0,0 +1,98 @@
+let Discord = require('discord.js');
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: 'giveaway',
+    execute(client, message){
+        if (!message.guild) return;
+        async function giveaway() {
+            var time = '';
+            var time2 = '';
+            var time3 = '';
+            if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You don\'t have enough permissions to use this command.');
+            if (message.content === `${prefix}giveaway`) return message.channel.send(`You didn\'t state a duration or a price for the giveaway.`)
+            if (message.content !== `${prefix}giveaway`) {
+                const stated_duration_hours = message.content.split(' ')[1];
+                const stated_duration_hours2 = stated_duration_hours.toLowerCase();
+                if (stated_duration_hours2.includes('s')) {
+                    var time = 's';
+                }
+                if (stated_duration_hours2.includes('m')) {
+                    var time = 'm';
+                }
+                if (stated_duration_hours2.includes('h')) {
+                    var time = 'h';
+                }
+                if (stated_duration_hours2.includes('d')) {
+                    var time = 'd';
+                }
+                const stated_duration_hours3 = stated_duration_hours2.replace(time, '');
+                if (stated_duration_hours3 === '0') {
+                    message.channel.send('The duration has to be atleast one.');
+                }
+                if (isNaN(stated_duration_hours3)) {
+                    message.channel.send('The duration has to be a valid time variable.');
+                }
+                if (stated_duration_hours3 > 1) {
+                    var time3 = 's';
+                }
+                if (time === 's') {
+                    var actual_duration_hours = stated_duration_hours3 * 1000;
+                    var time2 = 'second';
+                }
+                if (time === 'm') {
+                    var actual_duration_hours = stated_duration_hours3 * 60000;
+                    var time2 = 'minute';
+                }
+                if (time === 'h') {
+                    var actual_duration_hours = stated_duration_hours3 * 3600000;
+                    var time2 = 'hour';
+                }
+                if (time === 'd') {
+                    var actual_duration_hours = stated_duration_hours3 * 86400000;
+                    var time2 = 'day';
+                }
+                if (!isNaN(stated_duration_hours3)) {
+                    const photo = message.content.split(' ')[2];
+                    if (photo === '') return message.channel.send('You have to enter a photo.');
+                    const prize = message.content.split(' ').slice(3).join(' ');
+                    if (prize === '') return message.channel.send('You have to enter a price.');
+                    if (stated_duration_hours3 !== '0') {
+                        const embed = new Discord.MessageEmbed()
+                        .setTitle(`${prize}`)
+                        .setColor('36393F')
+                        .setDescription(`Kliknij w reakcje 👍 i wygraj ${prize}!\n `+"`Giveaway kończy się: `")
+						.setImage(photo)
+                        .setFooter(`Utworzony przez ${message.author.username}`)
+                        let msg = await message.channel.send(embed)
+                        await msg.react('👍')
+
+                        setTimeout(() => {
+                            msg.reactions.cache.get('👍').users.remove(client.user.id)
+                            setTimeout(() => {
+                                let winner = msg.reactions.cache.get('👍').users.cache.random();
+                                if (msg.reactions.cache.get('👍').users.cache.size < 1) {
+                                    const winner_embed = new Discord.MessageEmbed()
+                                    .setTitle(`${prize}`)
+                                    .setColor('36393F')
+                                    .setDescription(`Brak reakcji pod giveaway'em`)
+                                    message.channel.send(winner_embed);
+                                }
+                                if (!msg.reactions.cache.get('👍').users.cache.size < 1) {
+                                    const winner_embed = new Discord.MessageEmbed()
+                                    .setTitle(`**Ogłoszenie wyników**`)
+                                    .setColor('36393F')
+                                    .setDescription(`Giveaway o ***${prize}*** wygrał *${winner.tag}*\n**Gratulacje!**`)
+                                    .setFooter(`Liczba uczestników - ${msg.reactions.cache.get('👍').count}`)
+                                    message.channel.send(winner_embed);
+                                    message.channel.send(`Prosimy ${winner} o odebranie nagrody od ${message.author}`)
+                                }
+                            }, 1000);
+                        }, actual_duration_hours);
+                    }
+                }
+            }
+        }
+        giveaway();
+    }
+}
