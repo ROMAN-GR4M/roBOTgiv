3415085d521f591135c40054e0416788050b4cdb
ROMAN_GR4M <indexman1000@gmail.com>
2021-01-15 13:11:45 +0100

    Create reroll.js

---
+++ commands/reroll.js
@@ -0,0 +1,28 @@
+let Discord = require('discord.js');
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: 'giveaway',
+    execute(client, message){
+        
+        setTimeout(() => {
+            let winner = msg.reactions.cache.get('👍').users.cache.random();
+            if (msg.reactions.cache.get('👍').users.cache.size < 1) {
+                const winner_embed = new Discord.MessageEmbed()
+                .setTitle(`${prize}`)
+                .setColor('36393F')
+                .setDescription(`Brak reakcji pod giveaway'em`)
+                message.channel.send(winner_embed);
+            }
+            if (!msg.reactions.cache.get('👍').users.cache.size < 1) {
+                const winner_embed = new Discord.MessageEmbed()
+                .setTitle(`**Ogłoszenie wyników**`)
+                .setColor('c27c0e')
+                .setDescription(`Giveaway o ***${prize}*** wygrał *${winner.tag}*\n**Gratulacje!**`)
+                .setFooter(`Liczba uczestników - ${msg.reactions.cache.get('👍').count}`)
+                message.channel.send(winner_embed);
+                message.channel.send(`Prosimy ${winner} o odebranie nagrody od ${message.author}`)
+            }
+        }, 1000);
+    }
+}
