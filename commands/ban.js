b99c5e235604c885557ebe7023b73b079ac1e5f7
ROMAN_GR4M <indexman1000@gmail.com>
2021-01-21 20:48:53 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,22 @@
 let Discord = require('discord.js');
 const { prefix } = require('../config.json');
 
-const embed = new Discord.MessageEmbed()
-    .setTitle('Ojojoj, chyba nie masz uprawnień')
-    .setDescription(`${message.author} próbował zbanować ${mentions.user}`)
-    .setTimestamp();
-const embed1 = new Discord.MessageEmbed()
-    .setTitle(`Poleciał BAN`)
-    .setDescription(`${toBan} został zbanowany!\nPowód: ${reason}`)
-    .setTimestamp();
+
 
 module.exports = {
     name: 'ban',
     description: 'siema',
         execute(client, message) {
             if(message.content === `${prefix}ban`) {
+            const embed = new Discord.MessageEmbed()
+                .setTitle('Ojojoj, chyba nie masz uprawnień')
+                .setDescription(`${message.author} próbował zbanować ${mentions.user}`)
+                .setTimestamp();
+            const embed1 = new Discord.MessageEmbed()
+                .setTitle(`Poleciał BAN`)
+                .setDescription(`${toBan} został zbanowany!\nPowód: ${reason}`)
+                .setTimestamp();
+
             const { member, mentions } = message
 
             let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
