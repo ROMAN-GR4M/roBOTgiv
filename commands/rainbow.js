83e75dd7cd2974d4a5e09e4cde828145cc61ddd3
ROMAN_GR4M <indexman1000@gmail.com>
2021-01-26 23:36:49 +0100

    Update rainbow.js

--- commands/rainbow.js
+++ commands/rainbow.js
@@ -5,17 +5,13 @@ module.exports = {
     name: 'rainbow',
     description: 'siema',
         execute(client, message) {
-            if(message.content === prefix+'rainbow'){
+            if(message.content === `${prefix}rainbow`){
                 if(!message.guild) return;
                 if(!message.guild.member(bot.user).hasPermission('MANAGE_ROLES')) return;
-                var colors = ['#8585ff','#fff681','#a073fd','#fd73b9'];
-                for(let i = 0; i<= colors.length;i++){
-                var role = message.guild.roles.find(role => role.name === "rolename");
-                setInterval(() => {
+                message.guild.roles.cache.find(role => role.name === "Admin");
                     role.edit({
-                        color: colors[i]
+                        color: '#8585ff'
                     })
-                }, 20000);
               }
             }
         }
