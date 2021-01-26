a2a2019d0a9c52c194df3545116b7ed6fbc0e0f4
ROMAN_GR4M <indexman1000@gmail.com>
2021-01-26 23:17:11 +0100

    Update rainbow.js

--- commands/rainbow.js
+++ commands/rainbow.js
@@ -8,9 +8,9 @@ module.exports = {
             if(message.content === prefix+'rainbow'){
                 if(!message.guild) return;
                 if(!message.guild.member(bot.user).hasPermission('MANAGE_ROLES')) return;
-                let colors = ['#8585ff','#fff681','#a073fd','#fd73b9'];
+                var colors = ['#8585ff','#fff681','#a073fd','#fd73b9'];
                 for(let i = 0; i<= colors.length;i++){
-                let role = message.guild.roles.find(role => role.name === "rolename");
+                var role = message.guild.roles.find(role => role.name === "rolename");
                 setInterval(() => {
                     role.edit({
                         color: colors[i]
