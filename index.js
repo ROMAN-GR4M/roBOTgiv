952d5b626450228ac9502b3f322bea2eea0435b8
ROMAN_GR4M <indexman1000@gmail.com>
2021-01-27 20:33:31 +0100

    dd

--- index.js
+++ index.js
@@ -3,6 +3,62 @@ const client = new Discord.Client();
 let fs = require('fs');
 const { prefix, token } = require('./config.json');
 
+const config = require('./config.json');
+const size = config.colors;
+const rainbow = new Array(size);
+
+for (var i = 0; i < size; i++) {
+  var red = sin_to_hex(i, 0 * Math.PI * 2 / 3);
+  var blue = sin_to_hex(i, 1 * Math.PI * 2 / 3);
+  var green = sin_to_hex(i, 2 * Math.PI * 2 / 3);
+
+  rainbow[i] = '#' + red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0' + hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {
+      let server = client.guilds.cache.get(servers[index]);
+      if (!server) {
+          if (config.logging) {
+              console.log(`[ColorChanger] Server ${servers[index]} was not found. Skipping.`);
+          }
+          continue;
+      }
+
+      let role = server.roles.cache.find(r => r.name === config.roleName);
+      if (!role) {
+          if (config.logging) {
+              console.log(`[ColorChanger] The role ${config.roleName} was not found on the server ${servers[index]}. Skipping.`);
+          }
+          continue;
+      }
+
+      role.setColor(rainbow[place]).catch(console.error);
+
+      if (config.logging) {
+          console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+      }
+  }
+
+  if (place == (size - 1)) {
+      place = 0;
+  } else {
+      place++;
+  }
+}
+
+
 const activity = [
   "Pamiętaj, by świętować godzinę papieską!",
   "Jestem twoim Bogiem, rozumiesz?",
@@ -29,6 +85,8 @@ client.on('ready', () => {
     if(i === activity.length) i = i - activity.length;
   }, 12000);
   
+  setInterval(changeColor, config.speed);
+  changeColor();
 });
 
 client.commands = new Discord.Collection();
