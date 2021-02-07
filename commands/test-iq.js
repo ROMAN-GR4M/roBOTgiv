350096ba25f38fe31fb4c5b9c37ea72f1ed772d2
ROMAN_GR4M <indexman1000@gmail.com>
2021-02-07 16:42:05 +0100

    d

---
+++ commands/test-iq.js
@@ -0,0 +1,29 @@
+let Discord = require('discord.js');
+const { prefix } = require('../config.json');
+
+function Random() {
+    let r = Math.floor(Math.random() * 102);
+    if(r == 101){
+        return 200;
+    }else return r;
+};
+function generateHex() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+module.exports = {
+    name: 'test-iq',
+    description: 'siema',
+        execute(client, message) {
+            if(message.content === `${prefix}test-iq`){
+            const embed = new Discord.MessageEmbed()
+            .setColor(generateHex())
+            .setTitle('TEST IQ')
+            .setDescription(`${message.author} posiada **`+Random()+` IQ**`)
+            .setFooter('Bot ma zawsze rację')
+            .setThumbnail(message.author.avatarURL())
+            .setTimestamp();
+            message.channel.send(embed);
+        }
+    }
+}
